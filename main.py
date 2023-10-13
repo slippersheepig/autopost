805565fe924bc6202c475bc0f90c2948f6eb15f2
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2023-10-13 15:22:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ This is the main file that runs the image display website.
 from flask import Flask, render_template
 from gevent.pywsgi import WSGIServer
 from image_scraper import ImageScraper
+from image_manager import ImageManager
 app = Flask(__name__, template_folder='.')
 @app.route('/')
 def display_images():
-    image_scraper = ImageScraper()
-    images = image_scraper.get_images()
+    image_manager = ImageManager()
+    images = image_manager.get_images()
     return render_template('index.html', images=images)
 if __name__ == '__main__':
+    image_scraper = ImageScraper()
+    image_scraper.scrape_images()
     http_server = WSGIServer(("0.0.0.0", 5000), app)
     http_server.serve_forever()
