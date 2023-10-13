1334443a4bd3eae4d2a2f201760dbcb2316932f2
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2023-10-13 15:25:20 +0800

    Update image_scraper.py

--- image_scraper.py
+++ image_scraper.py
@@ -1,31 +1,39 @@
 '''
-This file contains the ImageScraper class responsible for scraping and managing images.
+This file contains the ImageScraper class responsible for scraping images from the internet.
 '''
 import requests
+import os
 import random
+import time
 from bs4 import BeautifulSoup
-from datetime import datetime, timedelta
 class ImageScraper:
     def __init__(self):
-        self.images = []
-        self.last_update = None
-    def get_images(self):
-        if self.last_update is None or datetime.now() - self.last_update > timedelta(hours=8):
-            self.update_images()
-        return self.images
-    def update_images(self):
-        self.images = self.scrape_images()
-        self.last_update = datetime.now()
+        self.image_dir = 'images'
     def scrape_images(self):
-        url = 'https://images.google.com'
         query = '熊'
-        search_url = f'{url}/search?q={query}'
-        response = requests.get(search_url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        image_elements = soup.find_all('img')
+        url = f'https://images.google.com/search?q={query}&tbm=isch'
+        response = requests.get(url)
+        response.raise_for_status()
+        image_urls = self.extract_image_urls(response.text)
+        self.download_images(image_urls)
+    def extract_image_urls(self, html):
+        soup = BeautifulSoup(html, 'html.parser')
         image_urls = []
-        for img in image_elements:
-            if '熊' in img.get('alt', '').lower() or '熊' in img.get('title', '').lower() or '熊' in img.get_text().lower():
-                image_urls.append(img['src'])
-        random.shuffle(image_urls)
-        return image_urls[:10]
+        for img in soup.find_all('img'):
+            image_url = img.get('src')
+            if image_url:
+                image_urls.append(image_url)
+        return image_urls
+    def download_images(self, image_urls):
+        if not os.path.exists(self.image_dir):
+            os.makedirs(self.image_dir)
+        for i, image_url in enumerate(image_urls):
+            response = requests.get(image_url)
+            if response.status_code == 200:
+                image_path = os.path.join(self.image_dir, f'image_{i}.jpg')
+                with open(image_path, 'wb') as f:
+                    f.write(response.content)
+            else:
+                print(f'Failed to download image: {image_url}')
+                continue  # Skip to the next image URL
+            time.sleep(1)  # Delay to avoid overloading the server
